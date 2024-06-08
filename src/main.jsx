1fe37169e211be15863078d4044c0468e6fd99b1
Hasana0777 <137383538+Hasana0777@users.noreply.github.com>
2024-06-08 12:36:29 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 //import App from './App.jsx'
-//import "./index.css"  -> Card;
-//import Usercard  from './Usercard.jsx'
-//import "./Qrcode.css";
-//import { Qrcode } from './Qrcode.jsx';
-//import  Calculator  from './Calculator.jsx';
-import  Todo  from './Todo.jsx';
+import "./Qrcode.css";
+import { Qrcode } from './Qrcode.jsx';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <Todo/>
+  <Qrcode/>
   
 
    
