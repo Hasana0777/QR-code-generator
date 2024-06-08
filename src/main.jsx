de74444ce67ebd425ccace4317c931a9c23792ef
Hasana0777 <137383538+Hasana0777@users.noreply.github.com>
2024-06-08 12:34:48 +0530

    Add files via upload
    The React QR Code Generator is a simple and easy-to-use application built with React, allowing users to generate QR codes effortlessly. Whether you need to create QR codes for URLs, text, or any other data, this generator has you covered.

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+//import App from './App.jsx'
+//import "./index.css"  -> Card;
+//import Usercard  from './Usercard.jsx'
+//import "./Qrcode.css";
+//import { Qrcode } from './Qrcode.jsx';
+//import  Calculator  from './Calculator.jsx';
+import  Todo  from './Todo.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  <Todo/>
+  
+
+   
+  </React.StrictMode>
+);
